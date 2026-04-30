482919e53e97672410fab17e4b583a3c6b77b0e8
fynngraf <fynn.graf07@gmail.com>
2026-04-30 05:24:37 +0200

    generate experiment.json.j2 from exp.meta.json

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from jinja2 import Environment, FileSystemLoader
 
 #load metadata
 def load_exp_meta(exp_path: Path) -> dict:
-    # Immer den ganzen Dateipfad übergeben
-    meta_file = exp_path / "meta.json"
+    # complete path
+    meta_file = exp_path / "exp.meta.json"
 
     if not meta_file.exists():
         print(f"Metadata file does not exist: {meta_file} - use default")
@@ -19,25 +19,28 @@ def load_exp_meta(exp_path: Path) -> dict:
         }
     with open(meta_file, 'r') as _:   #encoding='utf-8'
         return json.load(_)
-# generate exp.json
-def generate_exp_template(env: Environment, meta: dict):
 
+# generate experiment.json.j2 from exp.meta.json
+def generate_exp_template(env: Environment, meta: dict, templates_dir: str, exp_id: str):
     template = """\
 {
-
-    "name": "{{ name }}",
-    "author": "{{ author }}",
-    "version": "{{ version }}",
-    "description": "{{ description }}",
-    "simulations": [ {{ all_simulations }} ],     
-    "datasets": [ {{ all_datasets }} ]           
+    "name":        \"""" + meta['name'] + """\",
+    "author":      \"""" + meta['author'] + """\",
+    "version":     \"""" + meta['version'] + """\",
+    "description": \"""" + meta['description'] + """\",
+    "simulations": [ {{ all_simulations }} ],
+    "datasets":    [ {{ all_datasets }} ]
 }
 """
+    # save experiment.json.j2
+    template_path = Path(templates_dir) / exp_id / "experiment.json.j2"
+    with open(template_path, "w") as f:
+        f.write(template)
+
     templ = env.from_string(template)
     return templ
 
 def parse_args():
-    #generates meta.json
     parser = argparse.ArgumentParser(
         description="Generate metadata.json for experiments"
     )
@@ -66,16 +69,10 @@ def parse_args():
 def main():
     args = parse_args()
 
-    exp_id = args.exp #e.g. "sdl_exp_309"
-    input_path = Path(args.sdl_dir) / exp_id #e.g. ./templates/sdl_exp_309
-    output_file = input_path / args.output_file
-    #sdl_exp_xyz = "sdl_exp_309"
+    exp_id = args.exp                            # e.g. "sdl_exp_309"
+    input_path = Path(args.sdl_dir) / exp_id     # e.g. ./sdl_exp_309
+    output_file = input_path / args.output_file  # e.g. ./sdl_exp_309/metadata.json
 
-    #set path for metadata json input
-    #meta_path = Path(f'templates/{sdl_exp_xyz}/exp_meta.json.j2')
-
-    #with open(meta_path, encoding='utf-8') as f:
-    #    meta = json.load(f)
     print(f'Experiment : {exp_id}')
     print(f'Input Path : {input_path}')
     print(f'Output File : {output_file}')
@@ -83,85 +80,70 @@ def main():
     if not input_path.exists():
         raise FileNotFoundError(f"Input path does not exist: {input_path}")
 
-    # meta = load_exp_meta(input_path / meta.json)
-    meta = load_exp_meta(input_path)
+    # read exp.meta.json from templates/sdl_exp_xyz/
+    meta = load_exp_meta(Path(args.templates_dir) / exp_id)
     print(f'metadata : {meta}')
 
     # set Jinja2 Environment
-    env= Environment(loader=FileSystemLoader(args.templates_dir))
+    env = Environment(loader=FileSystemLoader(args.templates_dir))
 
     # load templates
-    #templ_exp = env.get_template('templates/experiment.json.j2')
     templ_s_r = env.get_template('simulation.json.j2')
     templ_d_s = env.get_template('dataset.json.j2')
 
-    # generate exp.json from meta.json dynamically
-    templ_exp = generate_exp_template(env, meta)
+    # generate experiment.json.j2 from exp.meta.json dynamically
+    templ_exp = generate_exp_template(env, meta, args.templates_dir, exp_id)
 
     # write general information to metadata.json
-    # templ_exp only looks for all_simulations and all_datasets
-    all_s_r= ""
-    all_d_s= ""
+    all_s_r = ""
+    all_d_s = ""
 
     # go through all directories
-
     for item in sorted(input_path.rglob('*')):
-        # skip meta.json and output file
-        if item.name in ("meta.json", args.output_file):
+        # skip exp.meta.json and output file
+        if item.name in ("exp.meta.json", args.output_file):
             continue
 
         if item.is_dir():
             print(f"Generate simulation for: {item.name}")
-            # templ_s_r simulation run only looks for simulation_name
             if all_s_r:
-                all_s_r +=","
-            all_s_r += templ_s_r.render(simulation_name= item.name)
+                all_s_r += ","
+            all_s_r += templ_s_r.render(simulation_name=item.name)
 
         #  for all files add a dataset
         elif item.is_file():
             print(f"Generate dataset for: {item.name}")
             prefix = item.parent.name
             file_name = item.name
-            file_format = file_name.split(".")[-1]  # file_format = item.suffix.lstrip(".") or "unknown"
+            file_format = file_name.split(".")[-1]
             description = "Unknown"
-
-
-
-
 
             #  TODO: More elaborate descriptions and input|output|data product|etc
             #  needed for temp_d_s: file_name, prefix, file_format, description, file_type
 
-            #set file_type from path_info
+            # set file_type from path_info
             if "input" in str(item).lower():
                 file_type = "input"
             else:
                 file_type = "output"
 
-            if all_d_s != "": all_d_s+=","
-            all_d_s+= templ_d_s.render(
-                prefix= prefix,
-                file_name= file_name,
-                file_format= file_format,
-                description= description,
-                file_type= file_type
+            if all_d_s != "": all_d_s += ","
+            all_d_s += templ_d_s.render(
+                prefix=prefix,
+                file_name=file_name,
+                file_format=file_format,
+                description=description,
+                file_type=file_type
             )
-    #write meta.json
+
+    # write metadata.json
     with open(output_file, "w") as f:
         f.write(templ_exp.render(
             **meta,
-            all_simulations= all_s_r,
-            all_datasets= all_d_s)
+            all_simulations=all_s_r,
+            all_datasets=all_d_s)
         )
     print(f"\nfinished, metadata written to: {output_file}")
 
 if __name__ == '__main__':
     main()
-
-# TODO: Use argparse
-# parser = argparse.ArgumentParser(description="create metadata.json file from SeisSol folder")
-# parser.add_argument("--path", nargs=1, metavar=("input_path"), default=("./SeisSol_Output"), help="path to SeisSol folder used as input", type=str)
-# parser.add_argument("--output-file", nargs=1, metavar=("output_file"), default=("metadata.json"), help="output file", type=str)
-# args = parser.parse_args()
-# print(f"input_path: {input_path}")
-# print(f"output_file: {output_file}")
